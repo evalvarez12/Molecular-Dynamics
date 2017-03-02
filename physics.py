d1cf3f25f0e9ce9c1a782c87f2501dacd20648e4
Eduardo <evalvarez12@gmail.com>
2017-03-02 14:47:19 +0100

    Changed vecs initialization in phys.

--- physics.py
+++ physics.py
@@ -2,37 +2,6 @@ import numpy as np
 from scipy.spatial.distance import pdist, squareform
 
 
-
-
-def normal_vec_2d_old(r, L) :
-    # TODO optimize min distance
-    # D is matrix containing the distances from a point to all points in each row
-    D = np.zeros((len(r),len(r)))
-    vecs = np.zeros((len(r),len(r),2))
-
-    temp = np.zeros((9,len(r),2))
-    virtual = np.array([[0,L],[0,-L],[L,0],[-L,0],[L,L],[L,-L],[-L,L],[-L,-L],[0,0]])
-
-
-    for i in range(len(r)) :
-        vecs[i] = r[i] - r
-        for v in range(len(virtual)) :
-            temp[v] = vecs[i] + virtual[v]
-        D[i] = np.min(np.linalg.norm(temp, axis = 2), axis = 0 )
-        ind = np.argmin(np.linalg.norm(temp, axis = 2), axis = 0 )
-        vecs[i] += virtual[ind]
-
-    D[D == 0] = 1
-    vecsx = vecs[:,:,0]
-    vecsy = vecs[:,:,1]
-
-    # rx and ry are matrices contining the normalized vectors from a point to the rest in each row
-    rx = np.divide(vecsx,D)
-    ry = np.divide(vecsy,D)
-    return rx, ry, D
-
-
-
 def normal_vec_2d(r, L) :
     # TODO optimize min distance
     # D is matrix containing the distances from a point to all points in each row
@@ -41,6 +10,7 @@ def normal_vec_2d(r, L) :
 
     for i in range(len(r)) :
         vecs[i] = r[i] - r
+    # vecs = np.array([r - i for i in r])
 
     vecs = ( vecs + L/2. ) % L - L/2.
     vecs[vecs > L/2.] *= -1
